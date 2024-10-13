a3c00701c17542d9300632b22f2564d7e7a774f7
Vishal101022 <raivishal939@gmail.com>
2024-10-13 18:33:01 +0530

    Add route

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello World");
-})
+  switch (req.url) {
+    case "/":
+      res.end("Welcome to our home page");
+      break;
+    case "/about":
+      res.end("Welcome to About us page");
+      break;
+    case "/node":
+      res.end("Welcome to my Node.js project");
+      break;
+    default:
+      res.end("Page not found");
+  }
+});
 
 server.listen(4000, () => {
-    console.log("Listening on port 4000");
-})
+  console.log("Listening on port 4000");
+});
